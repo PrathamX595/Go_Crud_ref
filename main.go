66b34e36ca0033fa62a006fad43b21df94f6d995
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-26 21:12:33 +0530

    setting env

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	godotenv.Load()
+	connectionstr:=os.Getenv("MONGO")
+	fmt.Println(connectionstr)
 }
