5c89f4aad65d142eb61e835c338171697c73d565
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-27 23:23:11 +0530

    checkes for env

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	router "crud/routes"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
+	mongo := os.Getenv("MONGO")
+	fmt.Println(mongo)
 	r := router.Router()
 	http.ListenAndServe(":4000", r)
 }
