2b399d48ff049bf88c5cbaa00528985aba73d365
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-30 01:51:50 +0530

    added update controller

--- controller/update.go
+++ controller/update.go
@@ -1,8 +1,23 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"crud/config"
+	"crud/utils"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func Update(c *fiber.Ctx) error {
+	id := c.Params("id")
+	userId, err := primitive.ObjectIDFromHex(id)
+	utils.CheckErr(err)
+	filter := bson.M{"_id": userId}
+	update := bson.M{"$set": bson.M{"verified": true}}
+	res, err := config.Col.UpdateOne(context.Background(), filter, update)
+	utils.CheckErr(err)
 
-	return c.JSON("")
+	return c.Status(200).JSON(res.ModifiedCount)
 }
--- models/user.go
+++ models/user.go
@@ -4,6 +4,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Password string             `json:"movie"`
-	Verified bool               `json:"-"`
+	Password string             `json:"-"`
+	Verified bool               `json:"verified"`
 }
