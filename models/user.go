f8d8f19c9db53054615ddfbf9d558d7ce34b05e3
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-30 02:11:32 +0530

    changes after final tests

--- models/user.go
+++ models/user.go
@@ -4,6 +4,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Password string             `json:"-"`
+	Password string             `json:"password"`
 	Verified bool               `json:"verified"`
 }
