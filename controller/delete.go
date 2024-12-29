368007e143281cad1eb5706afb316140dab98c9b
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-30 01:56:02 +0530

    added delete controller

--- controller/delete.go
+++ controller/delete.go
@@ -1,15 +1,30 @@
 package controller
 
 import (
+	"context"
+	"crud/config"
+	"crud/utils"
+
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func Delete(c *fiber.Ctx) error {
+	id := c.Params("id")
 
-	return nil
+	userId, err := primitive.ObjectIDFromHex(id)
+	utils.CheckErr(err)
+	filter := bson.M{"_id": userId}
+	res, err := config.Col.DeleteOne(context.Background(), filter)
+	utils.CheckErr(err)
+
+	return c.Status(200).JSON(res)
 }
 
 func DeleteAll(c *fiber.Ctx) error {
+	res, err := config.Col.DeleteMany(context.Background(), bson.D{{}}, nil)
+	utils.CheckErr(err)
 
-	return nil
+	return c.Status(200).JSON(res)
 }
