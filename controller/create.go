fb36a05259c6119c713514f994f2ff67a4078d41
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-30 01:47:14 +0530

    added create controller

--- controller/create.go
+++ controller/create.go
@@ -1,8 +1,20 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"crud/config"
+	"crud/models"
+	"crud/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Create(c *fiber.Ctx) error {
+	var filter models.User
+	err := c.BodyParser(&filter) //takes input from body
+	utils.CheckErr(err)
+	insert, err := config.Col.InsertOne(context.Background(), filter)
+	utils.CheckErr(err)
 
-	return c.JSON("")
+	return c.Status(200).JSON(insert)
 }
